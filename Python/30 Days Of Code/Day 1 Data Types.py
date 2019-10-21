28ed0a50a26c102316bc936180123f327ac48e75
shinzxc <alexdowell91@gmail.com>
2019-10-21 19:23:28 +0300

    add day 1

---
+++ Python/30 Days Of Code/Day 1 Data Types.py
@@ -0,0 +1,21 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+I2 = input()
+
+D2 = input()
+
+S2 = input()
+
+intVar = int(I2)
+
+intVar1 = float(D2)
+
+intVar2 = str(S2)
+
+print (intVar + i)
+
+print (intVar1 + d)
+
+print (s + intVar2)
